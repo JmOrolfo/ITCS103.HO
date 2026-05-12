2a74ce86fd4355dcd0890149087ec815b1d0c0bc
JmOrolfo <orolfojm08@gmail.com>
2026-05-12 11:30:56 +0800

    Add files via upload
    HANDSON

--- openpy.py
+++ openpy.py
@@ -8,11 +8,11 @@ def display():
     wb = op.load_workbook("excelDb.xlsx")
     sheet = wb.active
 
-    for row in tree.get_children():
-        tree.delete(row)
+    for row in table.get_children():
+        table.delete(row)
 
     for row in sheet.iter_rows(min_row=2,values_only=True):
-        tree.insert("", tk.END,values=row)
+        table.insert("", tk.END,values=row)
 
 def input_validation():
     first = fname_entry.get()
@@ -38,7 +38,7 @@ def saving():
     last = lname_entry.get()
     by = int(birth_entry.get())
 
-    age= 2026 -by
+    age= 2026 - by
     workbook = op.load_workbook("excelDb.xlsx")
     sheet = workbook.active
 
@@ -48,6 +48,78 @@ def saving():
     workbook.save("excelDb.xlsx")
 
     messagebox.showinfo("Success","Record added successfully!")
+
+def auto_pup(event):
+    selected = table.focus()
+    values = table.item(selected, "values")
+
+    if values:
+        fname_entry.delete(0,tk.END)
+        mname_entry.delete(0,tk.END)
+        lname_entry.delete(0,tk.END)
+        birth_entry.delete(0,tk.END)
+
+        fname_entry.insert(0,values[1])
+        mname_entry.insert(0,values[3])
+        lname_entry.insert(0,values[2])
+        birth_entry.insert(0,values[4])
+
+def updt():
+    selected = table.focus()
+
+    if not selected:
+        messagebox.showerror("Error", "Select a record first.")
+
+    if not input_validation:
+        return
+    
+    values = table.item(selected, "values")
+    rec_id = values[0]
+    first = fname_entry.get()
+    middle = mname_entry.get()
+    last = lname_entry.get()
+    birth = int(birth_entry.get())
+
+    age = 2026 - birth
+
+    workbook = op.load_workbook("excelDb.xlsx")
+    sheet = workbook.active
+
+    for rows in sheet.iter_rows(min_row = 2):
+        if str(rows[0].value) == str(rec_id):
+            rows[1].value = last
+            rows[2].value = first
+            rows[3].value = middle
+            rows[4].value = birth
+            rows[5].value = age
+        
+    workbook.save("excelDb.xlsx")
+    messagebox.showinfo("Updated Successfully.")
+    display()
+
+def delt():
+    selected = table.focus()
+
+    if not selected:
+        messagebox.showerror("Error", "Select a record first.")
+
+    values = table.item(selected,"values")
+    rec_id = values[0]
+
+    confirm = messagebox.askyesnocancel("Confirm","Are you sure you want to delete this record?")
+    if not confirm:
+        return
+    
+    workbook = op.load_workbook("excelDb.xlsx")
+    sheet = workbook.active
+
+    for i, row in enumerate(sheet.iter_rows(min_row=2), start = 2):
+        if str(row[0].value) == str(rec_id):
+            sheet.delete_rows(i)
+            break
+
+    workbook.save("excelDb.xlsx")
+    messagebox.showinfo("Successfu;", "Record deleted successfully.")          
 
 window=tk.Tk() 
 window.title("Age Calculator")
@@ -90,19 +162,21 @@ birth_entry.grid(row=4, column=1,columnspan=2,padx=(10,0))
 birthyear_label = tk.Label(genframe, text="Birth Year", font=("Poppins",10,"italic"),bg="lightgreen")
 birthyear_label.grid(row=5, column=1,columnspan=2)
 
-update_btn = tk.Button(window, text="Update")
+update_btn = tk.Button(window, text="Update", command=updt)
 update_btn.grid(row=6, column=2)
 
 button= tk.Button(window,text="Submit",font=("Poppins",12,"bold"),bg="lightpink", command=saving)
 button.grid(row=6, column=0, columnspan=6,pady=(10,20))
 
-delete_btn = tk.Button(window, text="Delete",  bg="red", fg="white")
+delete_btn = tk.Button(window, text="Delete",  bg="red", fg="white", command = delt)
 delete_btn.grid(row=6, column=3)
 
-tree = ttk.Treeview(window, columns=("ID","Last","First","Middle","BirthYear","Age"), show="headings")
+table = ttk.Treeview(window, columns=("ID","Last","First","Middle","BirthYear","Age"), show="headings")
 for headings in ("ID","Last","First","Middle","BirthYear","Age"):
-    tree.heading(headings, text=headings)
-tree.grid(row=7, column=0, columnspan=4)
+    table.heading(headings, text=headings)
+table.grid(row=7, column=0, columnspan=4)
+
+table.bind("<<TreeviewSelect>>", auto_pup)
 
 display()
 window.mainloop()
